d50bc3e5bf618535ced95eea3fb9de86433d12c2
Zakhozhij <maxzahojii@mail.ru>
2023-05-12 15:51:01 +0300

    Flights 18

--- dist/js/index.js
+++ dist/js/index.js
@@ -14,11 +14,9 @@ window.addEventListener(
 
 function getOffset() {
 	const form_position = document.querySelector(".company-partners").getBoundingClientRect().bottom;
-	const promo_position = document.querySelector(".promo").getBoundingClientRect().bottom;
-	document.querySelector(".info_block").style.top = form_position - promo_position + 35 + "px";
-	document.querySelector("footer").style.marginTop = form_position - promo_position + 35 + 240 + "px";
-	// document.querySelector(".elipse").style.top = flyer_club_position.top-10 + "px";
-	// document.querySelector(".elipse").style.right = flyer_club_position.left + 130 + "px";
+	const promo_position = document.querySelector(".promo").getBoundingClientRect();
+	document.querySelector(".info_block").style.marginTop = form_position - promo_position.bottom + 35 + "px";
+	document.querySelector("footer").style.marginTop = form_position - promo_position.bottom + 35 + 200 + "px";
 }
 
 
